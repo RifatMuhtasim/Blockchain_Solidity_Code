b0b79c60ccf0783a91a0587845acac0bfb4bc3a2
Rifat Muhtasim <67732890+RifatMuhtasim@users.noreply.github.com>
2023-09-30 16:39:35 +0600

    Update 1.01.Hello_Solidity.sol

--- Solidity_Code/1.01.Hello_Solidity.sol
+++ Solidity_Code/1.01.Hello_Solidity.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+
+contract HelloWorld{
+    string message;
+
+    constructor(){
+        message = "Hello Solidity";
+    }
+
+    function show_message() public view returns (string memory) {
+        return message;
+    }
+}
